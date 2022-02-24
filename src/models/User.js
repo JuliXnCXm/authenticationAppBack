ee6f330376a0ded65db973a0586c70bfa4aaa204
julian garcia <juliancamilogarciaescobar7@gmail.com>
2022-02-23 21:13:46 -0500

    make it

--- src/models/User.js
+++ src/models/User.js
@@ -2,19 +2,16 @@ const joi = require('joi');
 const {Schema, model, Collection} = require('mongoose');
 
 const UserSchema = joi.object().keys({
-    email: joi.string().email().required(),
-    password: joi
-        .string()
-        .regex(new RegExp('^[a-zA-Z0-9]{8,32}$'))
-        .required(),
-    name: joi.string().required(),
-    lastname: joi.string(),
-    provider: joi.string().required(),
-    description: joi.string(),
-    phone: joi.number(),
-    createdAt: joi.date().required(),
-    picture: joi.string(),
-    updatedAt: joi.date(),
+  email: joi.string().email().required(),
+  password: joi.string().regex(new RegExp("^[a-zA-Z0-9]{8,32}$")).required(),
+  name: joi.string().required(),
+  lastname: joi.string(),
+  provider: joi.string().required(),
+  description: joi.string().optional().allow("").min(10).max(10),
+  phone: joi.number().optional().allow("").min(10).max(10),
+  createdAt: joi.date().required(),
+  picture: joi.string(),
+  updatedAt: joi.date(),
 });
 
 const createUser = new Schema({
